94630e72bee360ca805dbc9445337999ac337312
Maksat Bairamov <141236499+MaksatBairamov@users.noreply.github.com>
2025-07-03 13:34:20 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getTransactions = async () => {
     try {
